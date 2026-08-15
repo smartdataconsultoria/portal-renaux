22d458115d9ba7a710191dddd00dd066ca44552d
smartdataconsultoria <tecnologia@smartdataconsultoria.com>
2026-08-15 19:01:21 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,8 @@
 const CONFIG = {
   SB_URL: 'https://rbahvyejqybajnufftio.supabase.co',
-  SB_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiYWh2eWVqcXliYWpudWZmdGlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwNjA5MjIsImV4cCI6MjA4OTYzNjkyMn0.nUIo2CT3o41YL1TT8Hrp5kq-_ZS-bMMf-gvUMCEyYMw'
+  SB_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiYWh2eWVqcXliYWpudWZmdGlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwNjA5MjIsImV4cCI6MjA4OTYzNjkyMn0.nUIo2CT3o41YL1TT8Hrp5kq-_ZS-bMMf-gvUMCEyYMw',
+  DIRETORES: [
+    'fabio@renauxservice.com.br',
+    'diego@renauxservice.com.br'
+  ]
 };
