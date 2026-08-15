e38f74a8cede201aeb54049d0eedbebd30c44724
Rafael <tecnologia@smartdataconsultoria.com>
2026-08-15 18:43:02 -0300

    Fix: atualiza chave Supabase correta

--- js/config.js
+++ js/config.js
@@ -1,5 +1,4 @@
-// Credenciais do Supabase
 const CONFIG = {
   SB_URL: 'https://rbahvyejqybajnufftio.supabase.co',
-  SB_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiYWh2ZWpxeWJham51ZmZ0aW8iLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc3NDA2MDkyMiwiZXhwIjoyMDg5NjM2OTIyfQ.nUIo2CT3o41YL1TT8Hrp5kq-_ZS-bMMf-gvUMCEyYMw'
+  SB_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiYWh2eWVqcXliYWpudWZmdGlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwNjA5MjIsImV4cCI6MjA4OTYzNjkyMn0.nUIo2CT3o41YL1TT8Hrp5kq-_ZS-bMMf-gvUMCEyYMw'
 };
