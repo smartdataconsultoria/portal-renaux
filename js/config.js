eb3a165f14531768855bc33fab0786f6e8ec65c9
smartdataconsultoria <tecnologia@smartdataconsultoria.com>
2026-08-15 22:25:05 -0300

    Add new email to DIRETORES in config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,7 @@ const CONFIG = {
   SB_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiYWh2eWVqcXliYWpudWZmdGlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwNjA5MjIsImV4cCI6MjA4OTYzNjkyMn0.nUIo2CT3o41YL1TT8Hrp5kq-_ZS-bMMf-gvUMCEyYMw',
   DIRETORES: [
     'fabio@renauxservice.com.br',
-    'diego@renauxservice.com.br'
+    'diego@renauxservice.com.br',
+    'tecnologia@smartdataconsultoria.com'
   ]
 };
